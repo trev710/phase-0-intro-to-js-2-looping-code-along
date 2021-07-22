c06daef94ce65d570aa6da811333a91516c871ca
trev710 <86570544+trev710@users.noreply.github.com>
2021-07-21 20:24:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,3 @@ function countDown(int) {
     }
       return int;
 }
-
-// for (let age = 30; age < 40; age++) {
-//     console.log(`I'm ${age} years old. Happy birthday to me!`);
-//     debugger;
-//   } 
