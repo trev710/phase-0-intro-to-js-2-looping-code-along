c9c1fbdbcb0e9b7a6df4f71ddcf0265a35f88dd7
TrevorJohnson <trevor.assetsolutions@gmail.com>
2022-03-15 10:00:46 -0600

    commit tree b34e2c351665b6710f1a07f449d148bb1a8215ea

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(name, event){ 
     let newArray =[];
-    for (let i=0; i<name.length;i++){
+    for (let i=0; i<=name.length;i++){
         newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
             }
     return newArray;
